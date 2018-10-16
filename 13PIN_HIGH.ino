a8856fda0b9e5b9c23129f324a8347a67784cb8e
SamuelJensen <44208290+SamuelJensen@users.noreply.github.com>
2018-10-16 13:38:13 -0400

    Add files via upload

---
+++ 13PIN_HIGH.ino
@@ -0,0 +1,17 @@
+const int ledPin =  5;  
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(ledPin, HIGH);
+
+    delay(500);
+
+    digitalWrite(ledPin, LOW);
+
+    delay(500);
+}
---
+++ Software_Controlled_LED.ino
@@ -0,0 +1,21 @@
+const int buttonPin = 2; 
+const int ledPin =  13;  
+int buttonState = 0; 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+ 
+    digitalWrite(ledPin, LOW);
+  } else {
+    
+    digitalWrite(ledPin, HIGH);
+  }
+
+}
